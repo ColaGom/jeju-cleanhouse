f9487123cf5cb05e9585368bf75efea93e444c7e
paca <paca@alphaca.kr>
2025-02-11 05:16:23 +0900

    fix: metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "제주도 클린하우스 찾기",
-  description: "Powered by Alphaca Labs, https://www.alphaca.kr/ 알파카랩스",
+  description: "제주도 클린하우스 위치 지도, 가까운 제주도 클린하우스, Powered by Alphaca Labs, https://www.alphaca.kr/ 알파카랩스",
 };
 
 export default function RootLayout({
